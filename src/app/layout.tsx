4e2d603e838ecf274b15ccca80ffcef476afabd6
Deivid0304 <lawrenvalderama23@gmail.com>
2026-06-27 22:16:06 +0800

    preview link image fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
         url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: "Arlan Dave & Rei Marie Anne Wedding Invitation",
+        alt: "Arlan Dave & Rei Marie Anne - Wedding Invitation",
       },
     ],
     locale: "en_US",
